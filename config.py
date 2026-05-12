0da929cd7844c0fef022c16432638efc646ff6b1
u8745-sys <u.8745@mail.ru>
2026-05-12 15:50:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ WEBHOOK_URL = None
 
 CATALOG = {
     "game_currency": {
-        "name": " Buhjdfz dfk.nf",
+        "name": " Игровая валюта",
         "items": {
             "val_1000": {"name": "1000 V-Bucks", "price": 500, "desc": "lkz Fortnite"},
             "lol_5000": {"name": "5000 RP", "price": 800, "desc": "lkz League of Legends"}
         }
     },
     "subscriptions": {
-        "name": " Gjlgbcrb",
+        "name": " Подписки",
         "items": {
-            "netflix_1m": {"name": "Netflix Premium 1 vtc", "price": 600, "desc": "4K, 4 ecnhjqcndf"},
-            "spotify_1m": {"name": "Spotify Premium 1 vtc", "price": 350, "desc": ",tp htrkfvs"}
+            "netflix_1m": {"name": "Netflix Premium 1 месяц", "price": 600, "desc": "4K, 4 ecnhjqcndf"},
+            "spotify_1m": {"name": "Spotify Premium 1 месяц", "price": 350, "desc": ",tp htrkfvs"}
         }
     }
 }
