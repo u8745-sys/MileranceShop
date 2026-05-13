b1f1e1c716f26be1f878cf54e636794a957115ee
u8745-sys <u.8745@mail.ru>
2026-05-13 13:28:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,9 @@
 import asyncio
-import threading
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from database import init_db
 from handlers import router
 from admin import router as admin_router
-from webhook import run_webhook
 
 async def main():
     init_db()
@@ -13,8 +11,6 @@ async def main():
     dp = Dispatcher()
     dp.include_router(router)
     dp.include_router(admin_router)
-    # Запускаем вебхук в отдельном потоке (только если файл webhook.py существует)
-    threading.Thread(target=run_webhook, daemon=True).start()
     print("Бот успешно запущен!")
     await dp.start_polling(bot)
 
